ddd1f1b9d1353fe19907ef2c8b20cea5958a1525
Dmitri Popov <dmpop@linux.com>
2015-02-27 13:51:06 +0100

    dn2820fykh-kubuntu

--- gphoto2-bottle.py
+++ gphoto2-bottle.py
@@ -14,7 +14,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from bottle import post, route, request, run
-import os, time
+import os, time, urllib
 
 # Camera-specific parameters
 param1 = '/main/capturesettings/f-number=f/'
@@ -35,6 +35,9 @@ def release_control():
         os.system('gphoto2 --set-config-value ' + param2 + shutterspeed)
         iso = request.forms.get('iso')
         os.system('gphoto2 --set-config-value ' + param3 + iso)
+    if (request.POST.get("command")):
+        cmd = urllib.unquote_plus(request.forms.get('cmd'))
+        os.system('gphoto2 ' + cmd)
     if (request.POST.get("start")):
         number = int(request.forms.get('number'))
         interval = int(request.forms.get('interval'))
@@ -47,32 +50,35 @@ def release_control():
     <title>gPhoto2 Bottle</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <form method="POST" action="/">
-    <div id="content"><p><input id="btn" name="shutter_release" type="submit" value="Shutter Release"></p>
+    <div id="content"><p><input id="btn" class="green" name="shutter_release" type="submit" value="Shutter Release"></p>
     <p class="right">Aperture (e.g., 9): <input name="aperture" type="text" size="3"/></p>
     <p class="right">Shutter speed (e.g. 1/125): <input name="shutterspeed" type="text" size="3"/></p>
     <p class="right">ISO (e.g., 640): <input name="iso" type="text" size="2"/></p>
     <p><input id="btn" name="set-config" value="Set" type="submit" /></p>
+    <p class="right">Command: <input name="cmd" type="text" size="25"/></p>
+    <p><input id="btn" name="command" value="Command" type="submit" /></p>
     <p class="right">Photos: <input name="number" type="text" size="3"/> Interval: <input name="interval" type="text" size="3"/> sec.</p>
-    <p><input id="btn" name="start" value="Start" type="submit" /></p>
-    <p><input id="btn" class="stop" name="stop" value="Stop" type="submit" /></p>
-    <p><input id="btn" class="shutdown" name="shutdown" value="Shut down" type="submit" /></p>
+    <p><input id="btn" class="green" name="start" value="Start" type="submit" /></p>
+    <p><input id="btn" class="orange" name="stop" value="Stop" type="submit" /></p>
+    <p><input id="btn" class="red" name="shutdown" value="Shut down" type="submit" /></p>
     </form>
-    <p class="justify">Press <strong>Shutter Release</strong> for a single shot.</p>
-    <p class="justify">Use the approprite fields to specify aperture, shutter speed, and ISO values. Press <strong>Set</strong> to apply the settings.</p>
-    <p class="justify">Use the appropriate fields to specify the number of photos and the interval between them in seconds, then press <strong>Start</strong>.</p>
-    <p class="justify">Press <strong>Stop</strong> to terminate the app.</p>
-    <p class="justify">Press <strong>Shutdown</strong> to shut down the server.</p>
+    <p class="left">Press <strong>Shutter Release</strong> for a single shot.</p>
+    <p class="left">Use the approprite fields to specify aperture, shutter speed, and ISO values. Press <strong>Set</strong> to apply the settings.</p>
+    <p class="left">Use the appropriate fields to specify the number of photos and the interval between them in seconds, then press <strong>Start</strong>.</p>
+    <p class="left">To run a user-defined command, specify the desired command in the <em>Command</em> field and press the <strong>Command</strong> button.</p>
+    <p class="left">Press <strong>Stop</strong> to terminate the app.</p>
+    <p class="left">Press <strong>Shutdown</strong> to shut down the server.</p>
     </div>
     <style>
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
     body {
         font: 15px/25px 'Open Sans', sans-serif;
     }
-    p.justify {
-        text-align: justify;
+    p.left {
+        text-align: left;
     }
-        p.right {
-    text-align: right;
+    p.right {
+        text-align: right;
     }
     #content {
         font: 15px/25px 'Open Sans', sans-serif;
@@ -89,10 +95,13 @@ def release_control():
         letter-spacing: 3px;
         border:none;
     }
-    #btn.stop {
+    #btn.green {
+        background: #009900;
+    }
+    #btn.orange {
         background: #ff9900;
     }
-    #btn.shutdown {
+    #btn.red {
         background: #cc0000;
     }
     </style>
