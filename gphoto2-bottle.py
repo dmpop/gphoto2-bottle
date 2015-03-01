3e70751f3ed730fe248a94c8c010ed5a2478eb6c
Dmitri Popov <dmpop@linux.com>
2015-03-01 18:43:26 +0100

    thinkpad-t410-kubuntu

--- gphoto2-bottle.py
+++ gphoto2-bottle.py
@@ -13,7 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from bottle import post, route, request, run
+from bottle import post, route, request, static_file, run
+from subprocess import Popen, PIPE
 import os, time, urllib
 
 try:
@@ -31,6 +32,10 @@ param2 = '/main/capturesettings/shutterspeed2='
 param3 = '/main/imgsettings/iso='
 # Default gPhoto2 command
 gphoto2_command = 'gphoto2 --capture-image-and-download --filename "%Y%m%d-%H%M%S-%03n.%C"'
+
+@route('/static/:path#.+#', name='static')
+def static(path):
+    return static_file(path, root='static')
 
 @route('/')
 @route('/', method='POST')
@@ -81,8 +86,12 @@ def release_control():
     return """
     <title>gPhoto2 Bottle</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="static/styles.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
     <form method="POST" action="/">
-    <div id="content"><p><input id="btn" class="green" name="shutter_release" type="submit" value="Shutter Release"></p>
+    <div id="content">
+    <p><a href="config">View config</a> | <a href="help">Help</a></p>
+    <p><input id="btn" class="green" name="shutter_release" type="submit" value="Shutter Release"></p>
     <p class="right">Aperture (e.g., 9): <input name="aperture" type="text" size="3"/></p>
     <p class="right">Shutter speed (e.g. 1/125): <input name="shutterspeed" type="text" size="3"/></p>
     <p class="right">ISO (e.g., 640): <input name="iso" type="text" size="2"/></p>
@@ -94,6 +103,29 @@ def release_control():
     <p><input id="btn" class="orange" name="stop" value="Stop" type="submit" /></p>
     <p><input id="btn" class="red" name="shutdown" value="Shut down" type="submit" /></p>
     </form>
+    </div>
+    """
+
+@route('/config')
+def config():
+    output = Popen(["gphoto2", "--list-config"], stdout=PIPE).communicate()[0]
+    return  """
+        <title>gPhoto2 Bottle Help</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="static/styles.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+        <p><a href="../">Back</a></p>
+        """ + output
+
+@route('/help')
+def help():
+    return """
+    <title>gPhoto2 Bottle Help</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="static/styles.css">
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+    <div id="content">
+    <p><a href="../">Back</a></p>
     <p class="left">Press <strong>Shutter Release</strong> for a single shot.</p>
     <p class="left">Use the approprite fields to specify aperture, shutter speed, and ISO values. Press <strong>Set</strong> to apply the settings.</p>
     <p class="left">Use the appropriate fields to specify the number of photos and the interval between them in seconds, then press <strong>Start</strong>.</p>
@@ -101,41 +133,6 @@ def release_control():
     <p class="left">Press <strong>Stop</strong> to terminate the app.</p>
     <p class="left">Press <strong>Shutdown</strong> to shut down the server.</p>
     </div>
-    <style>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
-    body {
-        font: 15px/25px 'Open Sans', sans-serif;
-    }
-    p.left {
-        text-align: left;
-    }
-    p.right {
-        text-align: right;
-    }
-    #content {
-        font: 15px/25px 'Open Sans', sans-serif;
-        margin: 0px auto;
-        width: 275px;
-        text-align: left;
-    }
-    #btn {
-        width: 11em;  height: 2em;
-        background: #3399ff;
-        border-radius: 5px;
-        color: #fff;
-        font-family: 'Open Sans', sans-serif; font-size: 25px; font-weight: 900;
-        letter-spacing: 3px;
-        border:none;
-    }
-    #btn.green {
-        background: #009900;
-    }
-    #btn.orange {
-        background: #ff9900;
-    }
-    #btn.red {
-        background: #cc0000;
-    }
-    </style>
     """
+
 run(host="0.0.0.0",port=8080, debug=True, reloader=True)
